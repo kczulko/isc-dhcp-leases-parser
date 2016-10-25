413ac127215696bc1d181ebc0c9f98c4b48aabac
kczulko <karol.czulkowski@gmail.com>
2016-10-25 20:13:49 +0200

    Added first simple test

--- src/test/scala/com/kczulko/isc/dhcp/grammars/IscDhcpLeasesGrammarTest.scala
+++ src/test/scala/com/kczulko/isc/dhcp/grammars/IscDhcpLeasesGrammarTest.scala
@@ -1,5 +1,6 @@
 package com.kczulko.isc.dhcp.grammars
 
+import com.kczulko.isc.dhcp.model._
 import org.scalatest.{FlatSpec, Inside, Matchers}
 
 class IscDhcpLeasesGrammarTest extends FlatSpec with Matchers with Inside {
@@ -10,7 +11,6 @@ class IscDhcpLeasesGrammarTest extends FlatSpec with Matchers with Inside {
     val entry =
       """
         |lease 110.31.40.13 {
-
         |  binding state active;
         |  next binding state free;
         |  rewind binding state free;
@@ -20,30 +20,23 @@ class IscDhcpLeasesGrammarTest extends FlatSpec with Matchers with Inside {
       """.stripMargin
 
     val result = grammar.parseAll(grammar.leases, entry)
-//
-//    result.get should contain theSameElementsAs List(
-//      "110.31.40.13" -> List(
-//        "starts" -> Map(
-//          "id" -> 2,
-//          "date" -> "2016/10/18",
-//          "time" -> "10:16:46"
-//        ),
-//        "ends" -> Map(
-//          "id" -> 2,
-//          "date" -> "2016/10/19",
-//          "time" -> "10:16:47"
-//        ),
-//        "cltt" -> Map(
-//          "id" -> 2,
-//          "date" -> "2016/10/20",
-//          "time" -> "10:16:48"
-//        ),
-//        "next-binding-state" -> "free",
-//        "binding-state" -> "active",
-//        "rewind-binding-state" -> "free",
-//        "hardware-ethernet" -> "54:ab:aa:36:b4:e1",
-//        "client-hostname" -> "\"other\""
-//      )
-//    )
+
+    result.successful shouldBe true
+    inside(result.get) { case Result(leases, serverDuid) =>
+        serverDuid shouldEqual None
+        leases should have length 1
+        leases should contain only (
+          Lease(
+            ip = Ip("110.31.40.13"),
+            bindingState = Some(BindingState("active")),
+            extendedBindingStates = List(
+              ExtendedBindingState("next", "free"),
+              ExtendedBindingState("rewind", "free")
+            ),
+            hardwareEthernet = Some(HardwareEthernet("54:ab:aa:36:b4:e1")),
+            clientHostname = Some(ClientHostname("\"other\""))
+          )
+        )
+    }
   }
 }
